65a49637f96c2708a708e148881f8d73dd7b31d4
MahDev0 <169010502+MahDev0@users.noreply.github.com>
2024-05-06 08:54:03 +0300

    Update ServerTeamFlag.lua

--- ServerTeamFlag.lua
+++ ServerTeamFlag.lua
@@ -1,13 +1,13 @@
-local teamModule = require(game.ReplicatedStorage.Main)
-teamModule.Start("Red","Blue")
-teamModule.FlagTouch()
-teamModule.CheckPlayerFlagDied()
-teamModule.PlayerLeft()
+local teamModule = require(game.ReplicatedStorage.Main) -- require module
+teamModule.Start("Red","Blue") -- start create game
+teamModule.FlagTouch() -- init functions and events
+teamModule.CheckPlayerFlagDied() -- players die event
+teamModule.PlayerLeft()  -- player left event
 
 game.Players.PlayerAdded:Connect(function(plr)
 	plr.CharacterAdded:Connect(function(chr)
 		chr.Humanoid.Died:Connect(function()
-			if chr.Humanoid:FindFirstChild("creator") then
+			if chr.Humanoid:FindFirstChild("creator") then --- check if player died from enemy team
 				
 				game.ReplicatedStorage.KillFeed:FireAllClients(chr.Humanoid:WaitForChild("creator").Value.Name, plr.Name ,plr.Team.Name,chr.Humanoid:WaitForChild("creator").Value.Team.Name)
 			else
